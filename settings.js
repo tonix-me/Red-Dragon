5497d4106a05a52c61daabe2723deb2019c4097b
Tonix <100058451+tonix-me@users.noreply.github.com>
2023-04-28 08:56:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,11 +29,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+919526235753'] // owner number
+global.ownername = "𝐓𝚶𝚴𝐈X" // owner name
 global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
-global.location = "India, Mizoram, Aizawl" // location
+global.socialm = "GitHub: TONIX" // github or insta name
+global.location = "India, INDONESIA, KERALA" // location
 
 //bot body 
 global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+919526235753']
+global.ownertag = '+919526235753' //your tag number
+global.botname = '𝐓𝚶𝚴𝐈𝐗 ⃝𝚩𝚯𝐓⛥' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "𝐓𝚶𝚴𝐈𝐗 ⃝𝚩𝚯𝐓⛥" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '𝐓𝚶𝚴𝐈𝐗 ⃝𝚩𝚯𝐓⛥' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+919526235753'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
